49a4c4112c22114842bd9a392390744bf54ed5e1
林榮顯 <68745388+lin-roger@users.noreply.github.com>
2024-03-27 20:22:11 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 // export const summary_api_url = "http://localhost:8000";
-export const summary_api_url = "http://140.125.84.43:8000";
-// export const summary_api_url = "https://1498-59-125-78-142.ngrok-free.app/";
+// export const summary_api_url = "http://140.125.84.43:8000";
+export const summary_api_url = "https://1498-59-125-78-142.ngrok-free.app/";
 export const api_url = "https://140.125.84.43:53622/api/";
 // export const api_url = "https://140.125.84.43/api/";
 // export const api_url = "https://rich-hopeful-dinosaur.ngrok-free.app/api/";
